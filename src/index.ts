40c540e9693373f474e32f1324a9debbf6026124
hbcarlos <carlosherrerocontact@gmail.com>
2022-01-07 15:56:11 +0100

    Add image to main area

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,24 @@
 import {
   JupyterFrontEnd,
-  JupyterFrontEndPlugin
+  JupyterFrontEndPlugin,
+  ILayoutRestorer
 } from '@jupyterlab/application';
 
-// import {
-//   ICommandPalette
-// } from '@jupyterlab/apputils';
-
 import {
-  Widget
-} from '@lumino/widgets';
+  ICommandPalette,
+  MainAreaWidget,
+  WidgetTracker
+} from '@jupyterlab/apputils';
 
-// import ImageEditor from 'tui-image-editor';
+import { Widget } from '@lumino/widgets';
+
+import ImageEditor from 'tui-image-editor';
 // import FileSaver from 'file-saver';
+
+namespace CommandIDs {
+  export const open = 'image-editor:open-editor';
+  export const openFile = 'image-editor:open-file';
+}
 
 /**
  * Initialization data for the image-editor extension.
@@ -20,33 +26,101 @@ import {
 const plugin: JupyterFrontEndPlugin<void> = {
   id: 'image-editor:plugin',
   autoStart: true,
-  activate: (app: JupyterFrontEnd) => {
+  requires: [ICommandPalette, ILayoutRestorer],
+  activate: (
+    app: JupyterFrontEnd,
+    palette: ICommandPalette,
+    restorer: ILayoutRestorer
+  ) => {
     console.log('JupyterLab extension image-editor is activated!');
 
-    const command = 'image-editor:open-editor';
+    let editorPanel: MainAreaWidget | null = null;
+    let imageEditor: ImageEditor | null = null;
 
-    app.commands.addCommand(command, {
+    // Namespace for the tracker
+    const namespace = 'documents-example';
+    // Creating the tracker for the document
+    const tracker = new WidgetTracker<MainAreaWidget>({ namespace });
+
+    // Handle state restoration.
+    if (restorer) {
+      // When restoring the app, if the document was open, reopen it
+      restorer.restore(tracker, {
+        command: CommandIDs.open,
+        name: widget => ''
+      });
+    }
+
+    app.commands.addCommand(CommandIDs.open, {
       label: 'Open Image Editor',
       caption: 'Open the Image Editor',
       isEnabled: () => true,
       execute: () => {
-
         const content = new Widget();
-        content.id = 'image-editor:main-image-editor';
-        // const instance = new ImageEditor(content.node, {
-        //   cssMaxWidth: 700,
-        //   cssMaxHeight: 500,
-        //   selectionStyle: {
-        //     cornerSize: 20,
-        //     rotatingPointOffset: 70,
-        //   }
-        // });
+        content.id = 'ImageEditor-Container';
+        editorPanel = new MainAreaWidget({ content });
+        editorPanel.title.label = 'Image Editor';
 
-        app.shell.add(content, 'main');
+        const node = document.createElement('input');
+        node.type = 'file';
+        node.onchange = (event: Event) => {
+          if (event) {
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            const file = event.target.files[0];
+            app.commands.execute(CommandIDs.openFile, { file });
+          }
+        };
+        const input = new Widget({ node });
+        editorPanel.toolbar.addItem('Open file into Image Editor', input);
 
-        // console.log(instance);
-      },
+        imageEditor = new ImageEditor(content.node, {
+          cssMaxWidth: 700,
+          cssMaxHeight: 500,
+          selectionStyle: {
+            cornerSize: 20,
+            rotatingPointOffset: 70
+          }
+        });
+
+        editorPanel.disposed.connect(() => {
+          imageEditor = null;
+          editorPanel = null;
+        });
+
+        app.shell.add(editorPanel, 'main');
+      }
     });
+
+    app.commands.addCommand(CommandIDs.openFile, {
+      label: 'Open file into Image Editor',
+      caption: 'Open file into Image Editor',
+      isEnabled: () => imageEditor !== null,
+      isVisible: () => imageEditor !== null,
+      execute: args => {
+        if (imageEditor !== null && args.file !== null) {
+          console.log(args);
+          imageEditor.loadImageFromFile(args.file as any).then(result => {
+            console.log(result);
+            if (imageEditor !== null) {
+              imageEditor.clearUndoStack();
+            }
+          });
+        }
+      }
+    });
+
+    if (palette) {
+      palette.addItem({
+        command: CommandIDs.open,
+        category: 'Image Editor'
+      });
+
+      palette.addItem({
+        command: CommandIDs.openFile,
+        category: 'Image Editor'
+      });
+    }
   }
 };
 
