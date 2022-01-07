f2a37b9fc2e9659dc27a136192a4acb3e3f149ee
Madhur Tandon <madhur16053@iiitd.ac.in>
2022-01-07 18:28:34 +0530

    experimenting with adding a new widget

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,17 @@ import {
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
 
+// import {
+//   ICommandPalette
+// } from '@jupyterlab/apputils';
+
+import {
+  Widget
+} from '@lumino/widgets';
+
+// import ImageEditor from 'tui-image-editor';
+// import FileSaver from 'file-saver';
+
 /**
  * Initialization data for the image-editor extension.
  */
@@ -11,6 +22,31 @@ const plugin: JupyterFrontEndPlugin<void> = {
   autoStart: true,
   activate: (app: JupyterFrontEnd) => {
     console.log('JupyterLab extension image-editor is activated!');
+
+    const command = 'image-editor:open-editor';
+
+    app.commands.addCommand(command, {
+      label: 'Open Image Editor',
+      caption: 'Open the Image Editor',
+      isEnabled: () => true,
+      execute: () => {
+
+        const content = new Widget();
+        content.id = 'image-editor:main-image-editor';
+        // const instance = new ImageEditor(content.node, {
+        //   cssMaxWidth: 700,
+        //   cssMaxHeight: 500,
+        //   selectionStyle: {
+        //     cornerSize: 20,
+        //     rotatingPointOffset: 70,
+        //   }
+        // });
+
+        app.shell.add(content, 'main');
+
+        // console.log(instance);
+      },
+    });
   }
 };
 
